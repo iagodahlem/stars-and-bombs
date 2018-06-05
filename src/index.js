6b1b03bc9cbc5f9a770801f24e2a91c06b5aa03d
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2018-06-05 11:53:47 -0300

    Loading assets

--- src/index.js
+++ src/index.js
@@ -15,11 +15,15 @@ const config = {
 const game = new Phaser.Game(config)
 
 function preload() {
-
+  this.load.image('sky', 'src/assets/sky.png')
+  this.load.image('ground', 'src/assets/platform.png')
+  this.load.image('star', 'src/assets/star.png')
+  this.load.image('bomb', 'src/assets/bomb.png')
+  this.load.spritesheet('dude', 'src/assets/dude.png', { frameWidth: 32, frameHeight: 48 })
 }
 
 function create() {
-
+  this.add.image(400, 300, 'sky')
 }
 
 function update() {
