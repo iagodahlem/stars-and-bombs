5d54fe825cc7fb5ae6a75632a85d97c7f871f40a
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2018-06-05 12:07:15 -0300

    Ready player one

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ const config = {
 const game = new Phaser.Game(config)
 
 let platforms
+let player
 
 function preload() {
   this.load.image('sky', 'src/assets/sky.png')
@@ -35,12 +36,34 @@ function create() {
   this.add.image(400, 300, 'sky')
 
   platforms = this.physics.add.staticGroup()
-
   platforms.create(400, 568, 'ground').setScale(2).refreshBody()
-
   platforms.create(600, 400, 'ground')
   platforms.create(50, 250, 'ground')
   platforms.create(750, 220, 'ground')
+
+  player = this.physics.add.sprite(100, 450, 'dude')
+  player.setBounce(0.2)
+  player.setCollideWorldBounds(true)
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  })
+
+  this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
+  })
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  })
 }
 
 function update() {
